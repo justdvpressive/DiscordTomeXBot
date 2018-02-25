6264cc333234b214d65e725b05a33ff5571cb89b
TomeXGalaxy <34920495+T0m3X@users.noreply.github.com>
2018-02-25 22:03:35 +0100

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -3,7 +3,7 @@ var prefix = "%";
 exports.run = (bot, message, args) => {
     if (message.content.startsWith(prefix+'avatar')) {
         if (!message.mentions.users.size) {
-            return message.channel.send(`bow: Ton Avatar : ${message.author.displayAvatarURL}`);
+            return message.channel.send(`:bow: Ton Avatar : ${message.author.displayAvatarURL}`);
         }
         const avatarList = message.mentions.users.map(user => {
             return `${user.username}'s Avatar : ${user.displayAvatarURL}`;
