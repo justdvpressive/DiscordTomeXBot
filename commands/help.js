cec6633e5d6e4eaac6f4fd4de4fe76496916e18e
TomeXGalaxy <34920495+T0m3X@users.noreply.github.com>
2018-02-26 11:53:57 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -8,7 +8,7 @@ exports.run = (bot, message, args) => {
         .setTitle(":pushpin: Guilde de TomeXBot ! :blue_book:")
         .setDescription("\n:warning: Respecte bien les Majuscules ! : ) \n\n:pencil: Voici toutes les catégories disponible : \n\n:page_with_curl: %help Info \n:wave: %help Fun \n:tools: %help Outils \n\n:flag_white: Langue : :flag_fr: \n:keyboard: Metode de dev utilisée : NodeJS Lien : https://nodejs.org \n:ok_hand: Le GitHub est Disponible :yum: https://github.com/T0m3X/TomeXBot/ :thumbsup:   \n\n:hammer_pick: Certaine commands ne sont pas dans le help car elle sont entrain d'être dev / modifié. Merci TomeX. :hammer_pick:")
         .setFooter(message.author.tag)
-        .setColor("#0099ff")
+        .setColor("#6600ff")
       });
     }
     if(args[0] === "Info") {
@@ -17,7 +17,7 @@ exports.run = (bot, message, args) => {
         .setTitle(":page_with_curl: Info")
         .setDescription("\n:warning: Respecte bien les Majuscules ! : ) \n\n:pencil: Voici toutes les commandes disponible sur cette catégorie : \n\n:printer: %stats \n:page_facing_up: %serverinfo \n:video_game: %osu [Pseudo] \n:game_die: %minecraft [Pseudo] \n\n:hammer_pick: Certaine commands ne sont pas dans le help car elle sont entrain d'être dev / modifié. Merci TomeX. :hammer_pick:")
         .setFooter(message.author.tag)
-        .setColor("#0099ff")
+        .setColor("#6600ff")
       })
     }
     if(args[0] === "Outils") {
@@ -26,7 +26,7 @@ exports.run = (bot, message, args) => {
         .setTitle(":tools: Outils")
         .setDescription("\n:warning: Respecte bien les Majuscules ! : ) \n\nVoici toutes les commandes disponible sur cette catégorie : \n\n:hammer: %ban \n:mag: %google [Indisponible]\n:minidisc: %rename [Indisponible] \n:x: :speech_balloon: %purge \n:rocket: %inviteserver \n:musical_note: %music [Indisponible] \n\n:hammer_pick: Certaine commands ne sont pas dans le help car elle sont entrain d'être dev / modifié. Merci TomeX. :hammer_pick:")
         .setFooter(message.author.tag)
-        .setColor("#0099ff")
+        .setColor("#6600ff")
       })
     }
     if(args[0] === "Fun") {
@@ -35,7 +35,7 @@ exports.run = (bot, message, args) => {
         .setTitle(":wave: Fun")
         .setDescription("\n:warning: Respecte bien les Majuscules ! : ) \n\n:pencil: Voici toutes les commandes disponible sur cette catégorie : \n\n:poop: %shit \n:camera: %gif [Un peu longue a des moments Désolé.]\n:8ball: %8ball \n:bow: %avatar \n\n:hammer_pick: Certaine commands ne sont pas dans le help car elle sont entrain d'être dev / modifié. Merci TomeX. :hammer_pick:")
         .setFooter(message.author.tag)
-        .setColor("#0099ff")
+        .setColor("#6600ff")
       })
     }
   }
