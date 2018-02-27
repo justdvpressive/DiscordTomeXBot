a94f5247e3641878cd4357e8e8be090e599abbe9
TomeXGalaxy <34920495+T0m3X@users.noreply.github.com>
2018-02-27 14:26:14 +0100

    Update shit.js

--- commands/shit.js
+++ commands/shit.js
@@ -1,16 +1,16 @@
+var prefix = "%"
 function makePass() {
     var text = "";
-    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789.-=+";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789.-=+_";
 
     for (var i = 0; i < 86; i++)
         text += possible.charAt(Math.floor(Math.random() * possible.length));
 
     return text;
 }
-
-var prefix = "%"
-
 exports.run = (bot, message, args) => {
-    if (message.content === prefix + "shit")
-     message.channel.send("Here your ramdom shit : \n" + makePass())
+    if (message.content === prefix + "shit") {
+     message.channel.send("Code alléatoire : \n" + makePass())
    }
+}
+
