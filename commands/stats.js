606e0d9dfd57f1f3ee8f291d50f2913ea24f20a5
TomeXCode <34920495+T0m3X@users.noreply.github.com>
2018-03-17 17:50:08 +0100

    Update stats.js

--- commands/stats.js
+++ commands/stats.js
@@ -13,27 +13,24 @@ if (message.content === prefix + "stats")
 
     \`  Username ${bot.user.tag}'
   
-=  Bot Statistics =
     
-=  Statistiques du TomeXBot / Serveur =
-      -> Total d'utilisateurs :: ${bot.guilds.reduce((p, c) => p + c.memberCount, 0).toLocaleString() } Utilisateurs
-      -> Total des bot :: ${bot.users.filter(user => user.bot).size.toLocaleString()} Bots
-      -> Je suis dans  :: ${bot.guilds.size.toLocaleString()} Serveurs
-      -> Total D'emojis (sur tout les servs ou je suis) :: ${bot.emojis.size.toLocaleString()} Emojis
-      -> Total des channels (sur tout les servs ou je suis) :: ${bot.channels.size.toLocaleString()} Channels
-=  Imformation du bot =
-      -> Ping ou le %ping :: ${Math.round(bot.ping)}ms
-=  Process Statistics =
-      -> Version du DiscordJS (du dev) :: v${Discord.version}
-      -> Version du NodeJS (du dev) :: ${process.version}
-      -> Bot allumée depuis :: ${moment.duration(process.uptime() * 1000).format(" D [days] H [hrs] m [mins] s [secs]")}
-      -> Client pret depuis :: ${moment.duration(bot.uptime).format(" D [days] H [hrs] m [mins] s [secs]")}
-=  Statisique du pc =
-      -> Systeme d'explotation :: ${require('child_process').execSync('ver').toString().trim()}
-      -> Pc du (du dev) Allumée depuis :: ${moment.duration(os.uptime(), "seconds").format(" D [days] H [hrs] m [mins] s [secs]")}
-      -> J'utilise :: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB
-      -> J'ai au total :: ${(process.memoryUsage().heapTotal / 1024 / 1024).toFixed(2)} MB
-      -> Ram du pc du (du dev) :: ${(os.totalmem() / Math.pow(1024, 3)).toLocaleString()} Gigabytes
-      -> Ram libre du (du dev) :: ${(os.freemem() / Math.pow(1024, 3)).toLocaleString()} Gigabytes`, {
+=  Server Infomations =
+\nTotal users :: ${bot.guilds.reduce((p, c) => p + c.memberCount, 0).toLocaleString() } Users
+Total bots :: ${bot.users.filter(user => user.bot).size.toLocaleString()} Bots
+I am in :: ${bot.guilds.size.toLocaleString()} Servers
+\n= Bot Informations =
+
+Ping :: ${Math.round(bot.ping)}ms
+\n=  Process Statistics =
+\nDiscordJS Version :: v${Discord.version}
+NodeJS Version :: ${process.version}
+Process uptime :: ${moment.duration(process.uptime() * 1000).format(" D [days] H [hrs] m [mins] s [secs]")}
+Client uptime :: ${moment.duration(bot.uptime).format(" D [days] H [hrs] m [mins] s [secs]")}
+\n=  Pc Informations =
+\nExplotation system :: ${require('child_process').execSync('ver').toString().trim()}
+Memory Usage :: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB
+Memory Heap :: ${(process.memoryUsage().heapTotal / 1024 / 1024).toFixed(2)} MB
+Total memory :: ${(os.totalmem() / Math.pow(1024, 3)).toLocaleString()} Gigabytes
+Free memory :: ${(os.freemem() / Math.pow(1024, 3)).toLocaleString()} Gigabytes`, {
               code: 'asciidoc'
      })}
