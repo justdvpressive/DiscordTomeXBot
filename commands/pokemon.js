d349ec5ad984f869a8088882ed01d2cb6f1b9f53
TotoDelon <30843697+TotoDelon@users.noreply.github.com>
2018-05-06 16:24:14 +0200

    Update pokemon.js
    Added ceriflor | Ajout de ceriflor

--- commands/pokemon.js
+++ commands/pokemon.js
@@ -1,4 +1,5 @@
 var Discord = require("discord.js")
+var prefix = "%"
 exports.run = (bot, message, args) => {
  if(!args[0]) {
    message.channel.send("", {
@@ -10,4 +11,15 @@ exports.run = (bot, message, args) => {
     .setDescription("Voici tout les pokemons disponible. \n\nCeriflor \n\nPachirisu \n\nArceus \n\nPikachu \n\nZigarde \n\n**Pour proposer un pokemon, faites pokemon propose {nom du pokemon}")
    })
   }
+ if(args[0] === "Ceriflor" || args[0] === "ceriflor") {
+  message.channel.send("", {
+        embed: new Discord.RichEmbed()
+        .setTitle("Qui est Ceriflor ?")
+        .setDescription("Ceriflor st un Pokémon de type Plante de la quatrième génération. Ceriflor est l'un des ambassadeurs de sa génération, il a été révélé pour la première fois dans le numéro du mois d'août 2006 du mensuel CoroCoro Comics, aux côtés de Pachirisu et Drascore.")
+        .setColor("#009933")
+        .setTitle("Qui est Ceriflor ?")
+        .setThumbnail("http://www.pokepedia.fr/images/2/26/Ceriflor-DP.png")
+        .setFooter("Demandée par : " + message.author.tag)
+      })
+ }
 }
